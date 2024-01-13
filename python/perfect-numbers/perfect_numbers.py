7efd9627820d9452930f1d5341cb8efb58c47fda
Bobby Pinard <bobby.pinard@gmail.com>
2024-01-13 12:13:23 -0600

    Perfect numbers completed

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,20 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if not number > 0:
+        raise ValueError("Classification is only possible for positive integers.")
+
+    factors = find_factors(number)[:-1]
+    if sum(factors) == number:
+        return "perfect"
+    elif sum(factors) > number:
+        return "abundant"
+    elif sum(factors) < number:
+        return "deficient"
+
+def find_factors(num):
+    factors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.append(i)
+    return factors
